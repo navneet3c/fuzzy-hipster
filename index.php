aa536558f1692d9911b973187141cf355b1be5f1
navo <navo.navneet@gmail.com>
2013-01-27 21:38:19 +0530

    main page started

--- index.php
+++ index.php
@@ -1,2 +1,47 @@
-<?php
-?>
+<html>
+<head>
+	<title>Nittfest13
+	</title>
+	<script src="files/jquery.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="files/index.js"></script>
+	<link href="files/index.css" rel="stylesheet" type="text/css">
+	<link href="assets/style.php" rel="stylesheet" type="text/css">
+</head>
+<body class="stretch">
+<div id="africa" class="stretch">
+<div id="back-night-div" class="background stretch"><img src="images/back_night.jpg" alt="" class="stretch backimage"></div>
+<div id="back-day-div" class="background stretch"><img src="images/back_day.jpg" alt="" class="stretch backimage"></div>
+<div id="container" class=" background stretch">
+
+<div id="logo"><img src="images/logo.png" alt=""></div>
+<div id="cloudy-container">
+<div id="cloud1" class="cloud"><img src="images/cloud1.png" alt=""></div>
+<div id="cloud2" class="cloud"><img src="images/cloud2.png" alt=""></div>
+<div id="cloud3" class="cloud"><img src="images/cloud1.png" alt=""></div>
+<div id="cloud4" class="cloud"><img src="images/cloud2.png" alt=""></div>
+<div id="cloud5" class="cloud"><img src="images/cloud1.png" alt=""></div>
+<div id="cloud6" class="cloud"><img src="images/cloud2.png" alt=""></div>
+<div id="cloud7" class="cloud"><img src="images/cloud1.png" alt=""></div>
+<div id="cloud8" class="cloud"><img src="images/cloud2.png" alt=""></div>
+<div id="sun"><img src="images/sun.png" alt=""></div>
+</div>
+<div id="bushy-container">
+<div id="bush1" class="bush"><img src="images/bush1.png" alt=""></div>
+<div id="bush2" class="bush"><img src="images/bush2.png" alt=""></div>
+<div id="bush3" class="bush"><img src="images/bush1.png" alt=""></div>
+<div id="bush4" class="bush"><img src="images/bush2.png" alt=""></div>
+<div id="bush5" class="bush"><img src="images/bush1.png" alt=""></div>
+<div id="bush-cover" class="bush"><img src="images/bush2.png" alt=""></div>
+</div>
+<div id="rockcrumb"><img src="images/rock.png" alt=""></div>
+<div id="tree"><img src="images/side_tree.png" alt=""></div>
+<div id="tribe1" class="tribe"><img src="images/tribe1.png" alt=""></div>
+<div id="tribe2" class="tribe"><img src="images/tribe2.png" alt=""></div>
+<div id="tribe3" class="tribe"><img src="images/tribe3.png" alt=""></div>
+<div id="tribe4" class="tribe"><img src="images/tribe4.png" alt=""></div>
+<div id="tribe5" class="tribe"><img src="images/tribe5.png" alt=""></div>
+
+</div>
+</div>
+</body>
+</html>
